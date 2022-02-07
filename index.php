ba4f69c395283dafd71929e37e67f025a9694850
unknown <you@example.com>
2022-02-07 08:03:08 -0300

    Adicionando arquivos e background

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/reset.css"/>
+    <link rel="stylesheet" href="./css/style.css"/>
+    <title>Math</title>
+</head>
+<body>
+    <header></header>
+    <main></main>
+    <footer>Copyright 2021 © | Florbela Freitas</footer>
+</body>
+</html>
