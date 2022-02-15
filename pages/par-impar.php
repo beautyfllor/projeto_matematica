d8d64a92024aecb395faf9da4f06203dcc9070b7
unknown <you@example.com>
2022-02-15 09:42:21 -0300

    mexi no par-impar

--- pages/par-impar.php
+++ pages/par-impar.php
@@ -1,23 +1,38 @@
 <?php
-    //Declaração das variáveis
+    //Validação para tratar se o botão foi clicado
+    if(isset($_POST['btnCalc'])) {
+
+        //Recebendo dados utilizando POST do formulário
+        $numInicial = $_POST['sltInicial'];
+        $numFinal = $_POST['sltFinal'];
+    }
     
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/par-impar.css">
-    <title>Document</title>
+    <title>Par ou Ímpar?</title>
 </head>
 <body>
     <form name="frmParImpar" method="post" action="par-impar.php">
         N° Inicial
-        <input type="text" name="txtNumInicial">
+        <select name="sltInicial">
+        <option value="" selected>Por favor selecione um número</option>
+        <?php
+            for($i = 0; $i <= 500; $i++) {
+                <option value=""><?=$i; ?></option>
+            }
+        ?>
+        </select>
         <br>
         N° Final
-        <input type="text" name="txtNumFinal">
-
+        <select name="sltFinal">
+        <option value="" selected> Por favor selecione um número</option>
+        </select>
+        <br>
         <input type="submit" name="btnCalc" value="Calcular">
 
         <div id="numPares">
