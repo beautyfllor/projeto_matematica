2c6263b99a746dbfefe162f708095b6f78a976da
unknown <you@example.com>
2022-02-16 08:13:32 -0300

    Problema dos selects resolvido

--- pages/par-impar.php
+++ pages/par-impar.php
@@ -1,10 +1,26 @@
 <?php
+    //Declaração das variáreis
+    $num1Acumulador = '<option>Por favor selecione um número</option>';
+    $num2Acumulador = '<option>Por favor selecione um número</option>';
+    $arrayPares = [];
+    $arrayImpares = [];
+
+    //Loopings para o select
+    for($i = 0; $i <= 500; $i++) {
+        $num1Acumulador .= '<option value"' . $i . '" >' . $i . '</option>';
+    }
+    for($i = 100; $i <= 1000; $i++) {
+        $num2Acumulador .= '<option value"' . $i . '" >' . $i . '</option>';
+    }
+
     //Validação para tratar se o botão foi clicado
     if(isset($_POST['btnCalc'])) {
 
         //Recebendo dados utilizando POST do formulário
         $numInicial = $_POST['sltInicial'];
         $numFinal = $_POST['sltFinal'];
+
+        
     }
     
 ?>
@@ -18,21 +34,11 @@
 </head>
 <body>
     <form name="frmParImpar" method="post" action="par-impar.php">
-        N° Inicial
-        <select name="sltInicial">
-        <option value="" selected>Por favor selecione um número</option>
-        <?php
-            for($i = 0; $i <= 500; $i++) {
-                <option value=""><?=$i; ?></option>
-            }
-        ?>
-        </select>
-        <br>
-        N° Final
-        <select name="sltFinal">
-        <option value="" selected> Por favor selecione um número</option>
-        </select>
-        <br>
+        <p>N° Inicial</p>
+        <select name="sltInicial"><?=$num1Acumulador; ?></select>
+        <p>N° Final</p>
+        <select name="sltFinal"><?=$num2Acumulador; ?></select>
+
         <input type="submit" name="btnCalc" value="Calcular">
 
         <div id="numPares">
